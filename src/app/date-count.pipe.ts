275d5976725ad714e84807a6d7ae341034788a80
briankiplimo <limobrian290@gmail.com>
2019-07-08 07:44:57 +0300

    insert the vote

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -6,12 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateCountPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today: Date = new Date(); //get current date and time
+    let today: Date = new Date();
     let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime)// returns value in milliseconds
-    const secondsInADay = 86400; //60 seconds*60 minutes in an hour *24 hours
+    var dateDifference = Math.abs(value - todayWithNoTime)
+    const secondsInADay = 86400;
 
-    var dateDifferenceSeconds = dateDifference * 0.001; //converts to seconds
+    var dateDifferenceSeconds = dateDifference * 0.001; 
 
     var dateCounter = dateDifferenceSeconds / secondsInADay;
 
